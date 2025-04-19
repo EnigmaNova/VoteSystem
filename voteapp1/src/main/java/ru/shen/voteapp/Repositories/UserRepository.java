cbc11b8481ab1ffa60594e173465af6d76525768
EnigmaNova <mikhailshening@gmail.com>
2025-04-19 13:22:10 +0300

    Delete voteapp1 directory

--- voteapp1/src/main/java/ru/shen/voteapp/Repositories/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package ru.shen.voteapp.Repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-import ru.shen.voteapp.Models.User;
-@Repository
-public interface UserRepository extends JpaRepository<User, Long>{
-}
